9da91f93d9555a9e438dcaa7fd2af0d41910ecd3
Tomáš Zlámal <tomaszlamal.github@gmail.com>
2024-02-19 13:46:55 +0100

    Created the Editor struct

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,31 @@
 use std::env;
+use std::io;
+use std::io::BufRead;
+struct Editor {
 
+}
+impl Editor {
+    fn start(&self, args: Vec<String>) {
+        if args.len() <= 1 {
+        // path with no inputted file
+            println!("Input the name and path of the file");
+            let mut input = &mut String::new();
+            let stdin = io::stdin();
+            stdin.read_line(input);
+            println!("You inputted: {}", input);
+            todo!();
+        }
+        else {
+            // path with a file in the params
+            let file = args.get(1);
+            todo!();
+        }
+    }
+}
 fn main() {
     let args: Vec<String> = env::args().collect();
-    //dbg!(args);
-    if args.len() <= 1  {
-        // path with no inputted file
-        todo!();
-    }
-    else {
-        // path with a file in the params
-        todo!();
-    }
+    let editor: Editor = Editor{};
+    editor.start(args);
+
+    
 }
